94653267e06eb2aa1147db2ae87e43809d374f14
16kenneyn <nick.kenney11@gmail.com>
2024-09-22 21:16:16 -0400

    sql server class update and HomeValues_2 update to point to NK server.

--- _utils.py
+++ _utils.py
@@ -1,6 +1,7 @@
 import pymssql
 
-def sql_server_query(input_query: str):
+# Define a function to query SQL Server
+def sql_server_query():
     # Define your connection parameters
     server = 'DESKTOP-UDT3OLM'  # \SQLEXPRESS
     user = 'sa'
@@ -9,16 +10,19 @@ def sql_server_query(input_query: str):
 
     # Establish connection
     conn = pymssql.connect(server=server, user=user, password=password, database=database)
-    cursor = conn.cursor()
+    sql_cursor = conn.cursor()
+
+    return sql_cursor
+
 
     # Query all tables
-    cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
+    # cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
 
     # Fetch and print all table names
-    tables = cursor.fetchall()
-    for table in tables:
-        print(table[0])
+#     tables = cursor.fetchall()
+#     for table in tables:
+#         print(table[0])
 
     # Close connection
-    cursor.close()
-    conn.close()
+#     cursor.close()
+#     conn.close()
